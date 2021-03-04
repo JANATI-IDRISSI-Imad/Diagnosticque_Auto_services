666f5c21516de97591496d558d8d0a81112c9a80
JANATI-IDRISSI-Imad <imad.janati.idrissi@gmail.com>
2021-03-04 19:54:40 +0100

    first commit

---
+++ src/main/java/ma/diagnosticque/auto/DiagnosticqueAutoServicesApplication.java
@@ -0,0 +1,13 @@
+package ma.diagnosticque.auto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosticqueAutoServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnosticqueAutoServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/diagnosticque/auto/DiagnosticqueAutoServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.diagnosticque.auto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosticqueAutoServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
